1e57ee028d8d84ebdc069b4aa3a504db6ae320db
Ardione321 <ardioned@gmail.com>
2025-02-12 23:17:35 +0800

    Add User Interface and Model

---
+++ backend/models/user.model.ts
@@ -0,0 +1,91 @@
+import { userRoles } from "@/constants/constants";
+import mongoose, { Document } from "mongoose";
+
+export interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  roles: string[];
+  profilePicture: {
+    id: string;
+    url: string | null;
+  };
+  password?: string | null;
+  authProviders: {
+    provider: string;
+    providerId: string;
+  }[];
+  subscription: {
+    id: string;
+    customer: string;
+    created: Date;
+    status: string;
+    startDate: Date;
+    currentPeriodEnd: Date;
+    nextPaymentAttempt: Date;
+  };
+}
+
+const authProviderSchema = new mongoose.Schema({
+  provider: {
+    type: String,
+    required: true,
+    enum: ["google", "github", "credentials"],
+  },
+  providerId: {
+    type: String,
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      trim: true,
+      unique: [true, "Email already exists"],
+      lowercase: true,
+    },
+    roles: {
+      type: [String],
+      default: ["user"],
+      enum: userRoles,
+    },
+    profilePicture: {
+      id: String,
+      url: {
+        type: String,
+        default: null,
+      },
+    },
+    password: {
+      type: String,
+      select: false,
+      minlength: [8, "Password must be at least 8 characters"],
+      default: null,
+    },
+    authProviders: {
+      type: [authProviderSchema],
+      default: [],
+    },
+    subscription: {
+      id: String,
+      status: String,
+      startDate: Date,
+      currentPeriodEnd: Date,
+      nextPaymentAttempt: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+export default User;
